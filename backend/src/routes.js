b26fff62743eb93c95ede0af385f278c0ad0ccbf
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-03-26 15:54:08 -0300

    feat: routes created

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const NgoController = require('./controllers/NgoController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.store);
+
+routes.get('/ngos', NgoController.index);
+routes.post('/ngos', NgoController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
