35faa4ae606b12bc3035a054c7448452a63f1175
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-03-30 16:45:55 -0300

    feat: logon page connected to the api

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +10,40 @@ import logo from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+	const [id, setId] = useState('');
+
+	const history = useHistory();
+
+	async function handleLogon(e) {
+		e.preventDefault();
+
+		try {
+			const response = await api.post('session', { id });
+
+			localStorage.setItem('ngoId', id);
+			localStorage.setItem('ngoName', response.data.name);
+
+			history.push('/profile');
+		} catch (error) {
+			alert('Falha no login, tente novamente');
+		}
+	}
+
 	return (
 		<div className="logon-container">
 			<section className="form">
 				<img src={logo} alt="Be the Hero" />
 
-				<form>
+				<form onSubmit={handleLogon}>
 					<h1>Faça seu logon</h1>
 
-					<input type="text" placeholder="Seu ID" />
+					<input
+						type="text"
+						placeholder="Seu ID"
+						value={id}
+						onChange={e => setId(e.target.value)}
+					/>
+
 					<button className="button" type="submit">Entrar</button>
 
 					<Link className="link" to="/register">
