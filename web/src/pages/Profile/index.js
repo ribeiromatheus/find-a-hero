f3f20483abc9bee946ea09e3e2df8b773fc97ba8
ribeiromatheus <ribeiro.matheuspereiraribeiro>
2020-03-31 15:20:20 -0300

    feat: logout a user

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -13,6 +13,8 @@ export default function Profile() {
 
   const ngoId = localStorage.getItem('ngoId');
   const ngoName = localStorage.getItem('ngoName');
+
+  const history = useHistory();
 
   useEffect(() => {
     api.get('profile', {
@@ -32,6 +34,12 @@ export default function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -39,7 +47,7 @@ export default function Profile() {
         <span>Bem-vinda {ngoName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button>
+        <button onClick={handleLogout}>
           <FiPower size={18} color={'#e02041'} />
         </button>
       </header>
